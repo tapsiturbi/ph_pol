dbfe6bc50cfb4e3a7be110e66ec6906a0bce892a
Levy Bajamundi <tapsiturbi@gmail.com>
2013-08-19 15:53:17 +0800

    Add insert SQL scripts to populate politicians and locations
    Created sql script with the insert statements to create the initial
    list of politicians and locations (province and municipality). Also
    updated db/seed.rb script to automatically run the sql scripts on
    rails.
    
    To run script on rails, run this:
    
    rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ActiveRecord::Base.connection.execute("delete from careers")
+ActiveRecord::Base.connection.execute("delete from politicians")
+ActiveRecord::Base.connection.execute("delete from locations")
+
+sql = File.open("db/seed_01_provincial.sql").read
+sql.split(';').each do |sql_statement|
+  sql_statement.strip!
+  if sql_statement.empty?
+    next
+  end
+  #puts sql_statement
+  ActiveRecord::Base.connection.execute(sql_statement)
+end
+
+sql = File.open("db/seed_02_municipal.sql").read
+sql.split(';').each do |sql_statement|
+  sql_statement.strip!
+  if sql_statement.empty?
+    next
+  end
+  ActiveRecord::Base.connection.execute(sql_statement)
+end
