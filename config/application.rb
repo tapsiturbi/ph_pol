14453b5d71bfd045f92c078916570fd228461bdc
Levy Bajamundi <tapsiturbi@gmail.com>
2013-11-25 23:07:35 +0800

    Fix IE issues on prod
    Fix IE issue where fonts weren't being loaded properly due to
    wrong URL prefix. Also fixed nav_menu image borders showing up
    on IE.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module PhPol
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    #config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # For Devise
     config.assets.initialize_on_precompile = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ PhPol::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_controller.asset_host = "http://poolitikahan.com"
 end
